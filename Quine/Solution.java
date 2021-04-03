9d067bbab7e756e735e6cc9fc7a29733d1e52315
Apar Ahuja <aparahuja123@gmail.com>
2021-04-04 02:38:38 +0530

    Final Docs

---
+++ Quine/Solution.java
@@ -0,0 +1,37 @@
+public class Solution
+{
+  public static void main(String[] blah)
+  {
+    char quotation = 34;
+    //System.out.println(Hi there! apar am Apar ahuja.);
+    String[] str = {
+    "public class Apar",
+    "{",
+    "  public static void main(String[] blah)",
+    "  {",
+    "    char quotation = 34;",
+    "    //System.out.println(Hi there! apar am Apar ahuja.);",
+    "    String[] str = {",
+    "    ",
+    "    }; int apar = 0;",
+    "    while(apar<7){",
+    "        System.out.println(str[apar]); apar++;}",
+    "    apar = 0;",
+    "    while(apar<str.length){",
+    "        System.out.println(str[7] + quotation + str[apar] + quotation + ','); apar++;}",
+    "    apar = 8;",
+    "    while(apar<str.length){",
+    "        System.out.println(str[apar]); apar++;}",
+    "  }",
+    "}",
+    }; int apar = 0;
+    while(apar<7){
+        System.out.println(str[apar]); apar++;}
+    apar = 0;
+    while(apar<str.length){
+        System.out.println(str[7] + quotation + str[apar] + quotation + ','); apar++;}
+    apar = 8;
+    while(apar<str.length){
+        System.out.println(str[apar]); apar++;}
+  }
+}
